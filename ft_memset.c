9261d0c11f7c59d7d14de68ecce25c6c08518860
sultaaaane <mohammed_bentahiralaoui@um5.ac.ma>
2023-10-19 12:56:44 +0100

    movit lfiles HHH

---
+++ ft_memset.c
@@ -0,0 +1,6 @@
+#include "libft.h"
+
+void ft_memset(void *str, int c, size_t n)
+{
+    
+}
---
+++ libft.h
@@ -0,0 +1,11 @@
+#ifndef LIBFT
+# define LIBFT
+
+#include <stdlib.h>
+#include <string.h>
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+
+
+#endif
