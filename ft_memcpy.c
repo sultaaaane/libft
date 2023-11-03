1d2c8db78c753ea52aad9c205002277940fb468f
sultaaaane <mohammed_bentahir@um5.ac.ma>
2023-11-03 17:44:07 +0100

    tttt

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mbentahi <mbentahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 16:11:24 by mbentahi          #+#    #+#             */
-/*   Updated: 2023/11/03 16:41:48 by mbentahi         ###   ########.fr       */
+/*   Updated: 2023/11/03 17:38:17 by mbentahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,12 @@
 void *memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
-	unsigned char *dst;
-	const unsigned char *src1;
 	
 	i = 0;
-	
-	while ()
+	while (i < n)
 	{
-		/* code */
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
 	}
-	
-	
+	return (dest);
 }
